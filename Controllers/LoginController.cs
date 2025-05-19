a409d3b1a98bb9a89207e25b7971a76ab74fa2b7
Ajith <ajithkma75ak@gmail.com>
2025-05-19 17:40:06 +0530

    Add files via upload

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KalyanamApp.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        public IActionResult Login(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                ViewBag.Error = "தயவுசெய்து உங்கள் பெயரை எழுதுங்கள்!";
+                return View();
+            }
+
+            HttpContext.Session.SetString("UserName", name);
+            return RedirectToAction("Invite");
+        }
+        [HttpGet]
+        public IActionResult Invite()
+        {
+            var name = HttpContext.Session.GetString("UserName");
+            if (string.IsNullOrEmpty(name)) return RedirectToAction("Login");
+
+            ViewBag.UserName = name;
+            return View();
+        }
+    }
+}
